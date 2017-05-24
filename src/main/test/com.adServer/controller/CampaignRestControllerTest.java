49d7b7d01332a388a239b4279de9595e94cf91db
dvenkatesh <venkatesh.duvvuri1@t-mobile.com>
2017-05-25 00:42:52 +0530

    Added files

---
+++ src/main/test/com.adServer/controller/CampaignRestControllerTest.java
@@ -0,0 +1,20 @@
+package com.adServer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.WebApplicationContext;
+
+/*@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = MyWebConfig.class)*/
+/*
+
+public class CampaignTestControllerTest {
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+private String a;
+
+
+
+}*/
